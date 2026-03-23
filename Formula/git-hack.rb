3c9f5729502b32d3ac88beea68f706ff1ffd1f0b
Kyle Klaus <kklaus@indemnity83.com>
2026-03-22 22:06:08 -0400

    chore(main): release 0.1.5 (#27)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/git-hack.rb
+++ Formula/git-hack.rb
@@ -1,12 +1,12 @@
 class GitHack < Formula
   desc "AI-augmented git workflow CLI using llm and git-town"
   homepage "https://github.com/indemnity83/homebrew-git-hack"
-  url "https://github.com/indemnity83/homebrew-git-hack/releases/download/v0.1.4/git-hack"
+  url "https://github.com/indemnity83/homebrew-git-hack/releases/download/v0.1.5/git-hack"
   sha256 "a5cb3a350e254c5de50f64329717f3e28774ccebe3f4e4184ca0ba0398dbd782"
   license "MIT"
 
   resource "manpage" do
-    url "https://github.com/indemnity83/homebrew-git-hack/releases/download/v0.1.3/git-hack.1"
+    url "https://github.com/indemnity83/homebrew-git-hack/releases/download/v0.1.5/git-hack.1"
     sha256 "d305c6512bb2f0141478dce5a34243494d73afed6b711bb60ff75ea365d4c886"
   end
 
